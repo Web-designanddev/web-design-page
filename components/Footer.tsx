bb7402499d4ca9ce073375d49a8542c773874710
Ebrahim Beiati-Asl <45979474+ebrahimbeiati@users.noreply.github.com>
2024-06-01 23:31:39 +0100

    new

---
+++ components/Footer.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Image from "next/image";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+import { socialMedia } from "@/data/en";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer id="footer">
+      <div className="w-full flex items-center justify-center absolute left-0 -bottom-72 min-h-96">
+        <Image
+          src="/footer-grid.svg"
+          alt="grid"
+          layout="fill"
+          objectFit="cover"
+          className="opacity-50"
+        />
+      </div>
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw]">
+          Let&apos;s build something great for
+          <span className="text-purple">You</span>
+        </h1>
+        <p
+          dir="ltr"
+          className="text-center flex items-center justify-center text-sm mt-4 mb-4 lg:max-w-[45vw]"
+        >
+          We are always open to discuss your project, idea or anything to get
+          you started.
+        </p>
+        <a href="mailto:webdesignanddev2@gmail.com">
+          <MagicButton
+            title="Contact Us"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+      <div
+        dir="ltr"
+        className="text-center mt-12 md:flex-row flex-col items-center flex justify-between text-sm lg:max-w-[45vw]"
+      >
+        <p className="md:text-base text-sm md:font-normal text-center flex items-center justify-center">
+          © {currentYear} Web Design and Development
+          <span className="text-purple">.</span>
+          All rights reserved.
+        </p>
+      </div>
+      <div className="flex items-center justify-center mt-4 md:gap-3 gap-6">
+        {socialMedia.map((info) => (
+          <a
+            key={info.id}
+            href={info.href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+          >
+            <Image src={info.img} alt="icons" width={20} height={20} />
+          </a>
+        ))}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/ui/FloatingNav.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  motion,
+  AnimatePresence,
+  useScroll,
+  useMotionValueEvent,
+} from "framer-motion";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+export const FloatingNav = ({
+  navItems,
+  className,
+}: {
+  navItems: {
+    name: string;
+    link: string;
+    icon?: JSX.Element;
+  }[];
+  className?: string;
+}) => {
+  const { scrollYProgress } = useScroll();
+
+  const [visible, setVisible] = useState(false);
+
+  useMotionValueEvent(scrollYProgress, "change", (current) => {
+    if (typeof current === "number") {
+      let direction = current! - scrollYProgress.getPrevious()!;
+
+      if (scrollYProgress.get() < 0.05) {
+        setVisible(false);
+      } else {
+        if (direction < 0) {
+          setVisible(true);
+        } else {
+          setVisible(false);
+        }
+      }
+    }
+  });
+
+  return (
+    <AnimatePresence mode="wait">
+      {visible && (
+        <motion.div
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -100, opacity: 0 }}
+          transition={{ duration: 0.2 }}
+          className={cn(
+            "flex max-w-fit fixed top-1 inset-x-0 mx-auto border rounded-full shadow-lg z-[5000] px-8 py-4 bg-white dark:bg-neutral-800 gap-4",
+            className
+          )}
+        >
+          {navItems.map((navItem, idx) => (
+            <Link
+              key={`link-${idx}`}
+              href={navItem.link}
+              className={cn(
+                "relative flex items-center space-x-2 text-neutral-600 dark:text-neutral-50 dark:hover:text-neutral-300 hover:text-neutral-800 transition-colors duration-200"
+              )}
+            >
+              <span>{navItem.icon}</span>
+              <span className="text-sm font-medium">{navItem.name}</span>
+            </Link>
+          ))}
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
